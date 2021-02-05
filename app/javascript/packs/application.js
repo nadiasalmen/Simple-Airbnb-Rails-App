6fbb8c02d45a2a85b06b0fa95510a677f12a5780
Nadia Salmen <nadiasalmen@gmail.com>
2021-02-05 13:11:46 -0300

    Frontend config and le wagon navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
+// External imports
+import 'bootstrap';
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
