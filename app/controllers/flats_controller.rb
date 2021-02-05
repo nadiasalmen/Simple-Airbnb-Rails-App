527c9748a123e1895826dac0430217245278ec48
Nadia Salmen <nadiasalmen@gmail.com>
2021-02-05 20:24:36 -0300

    Finish user story: As a user, I can add a flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,24 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+      # redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :addres, :description, :price_per_night, :number_of_guests)
+  end
 end
