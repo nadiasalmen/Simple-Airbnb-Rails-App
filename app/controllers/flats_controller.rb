a914bf30e30ccd3b90cad9c517e20472ae3510ac
Nadia Salmen <nadiasalmen@gmail.com>
2021-02-12 12:24:29 -0300

    User story: As a user, I can see detailed information of a given flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,6 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show]
+
   def index
     @flats = Flat.all
   end
@@ -17,9 +19,17 @@ class FlatsController < ApplicationController
     end
   end
 
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
   private
 
   def flat_params
     params.require(:flat).permit(:name, :addres, :description, :price_per_night, :number_of_guests)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
