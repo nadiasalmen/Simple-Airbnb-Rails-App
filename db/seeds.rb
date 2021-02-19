ae7c57a90fe6e5dc904ac830c92eb5e97043401b
Nadia Salmen <nadiasalmen@gmail.com>
2021-02-19 18:32:38 -0300

    Update seeds & sessions views

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,25 @@
 
 puts 'Cleaning database...'
 Flat.destroy_all
+User.destroy_all
+
+puts "Creating new users"
+
+user = User.new(
+  email: "nadia@airbnb.com",
+  password: "test1234",
+)
+user.save!
+
+user = User.new(
+  email: "ivan@airbnb.com",
+  password: "test1234",
+)
+user.save!
 
 puts "Creating new flats"
 
-flat = Flat.create!(
+flat = Flat.new(
   name: 'Amplio apartamento & terraza en Recoleta!',
   address: 'Recoleta, Buenos Aires, Argentina',
   description: 'Luminoso y amplio apartamento de 57m2 con una gran terraza con parrilla como muy pocos alojamientos en Buenos Aires pueden ofrecer.
@@ -21,9 +36,11 @@ flat = Flat.create!(
   number_of_guests: rand(2..4),
   url: "https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2546&q=80"
 )
-puts "Created flat with id #{flat.id}"
+flat.user = User.all.sample
+flat.save!
+puts "Created flat with id #{flat.id} - Belongs to user: #{flat.user.email}"
 
-flat = Flat.create!(
+flat = Flat.new(
   name: 'Dpto Remodelado a estrenar en Recoleta',
   address: 'Recoleta, Buenos Aires, Argentina',
   description: 'Buenos Aires es romanticismo y pasión. Esta maravillosa ciudad combina la grandeza europea con la pasión latina. Sexy y viva, Buenos Aires se mete por debajo de tu piel. La cuna del tango, y como este mismo la ciudad es, captivante, seductora y repleta de energía . Puedes disfrutar de todo esto y mas. Departamento re modelado a nuevo a estrenar, con estilo eco-moderno dentro de un hotel, justo en el corazón de Rec',
@@ -31,9 +48,11 @@ flat = Flat.create!(
   number_of_guests: rand(2..4),
   url: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2550&q=80"
 )
-puts "Created flat with id #{flat.id}"
+flat.user = User.all.sample
+flat.save!
+puts "Created flat with id #{flat.id} - Belongs to user: #{flat.user.email}"
 
-flat = Flat.create!(
+flat = Flat.new(
   name: 'Exclusive flat in the heart of Palermo Soho',
   address: 'Palermo, Buenos Aires, Argentina',
   description: 'Beautiful flat in the heart of the exclusive area of Palermo Soho. It has 1 double bed in the room and two single beds (sofa-bed) in the living room. The balcony is huge and has a very nice view of the city that you can appreciate in the pictures. Very bright and exclusive, Windows all over the property.',
@@ -41,9 +60,11 @@ flat = Flat.create!(
   number_of_guests: rand(2..4),
   url: "https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2550&q=80"
 )
-puts "Created flat with id #{flat.id}"
+flat.user = User.all.sample
+flat.save!
+puts "Created flat with id #{flat.id} - Belongs to user: #{flat.user.email}"
 
-flat = Flat.create!(
+flat = Flat.new(
   name: 'Monoambiente Único en Buenos Aires',
   address: 'San Nicolás, Buenos Aires, Argentina',
   description: 'Te invito a disfrutar de este hermoso monoambiente, un espacio cálido y con detalles únicos. Totalmente equipado. Está ubicado a media cuadra de la Plaza del Congreso, a 3 cuadras de Av. Corrientes y a 6 cuadras de Av. 9 de Julio. Súper luminoso y acogedor. Ubicación estratégica, a pocas cuadras de los más importantes teatros, cines y restaurantes de la ciudad.',
@@ -51,9 +72,11 @@ flat = Flat.create!(
   number_of_guests: rand(2..4),
   url: "https://images.unsplash.com/photo-1575517111478-7f6afd0973db?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2550&q=80"
 )
-puts "Created flat with id #{flat.id}"
+flat.user = User.all.sample
+flat.save!
+puts "Created flat with id #{flat.id} - Belongs to user: #{flat.user.email}"
 
-flat = Flat.create!(
+flat = Flat.new(
   name: 'Habitación privada en: departamento - Anfitrión: Mati & Cande',
   address: 'San Nicolás, Buenos Aires, Argentina',
   description: 'Enjoy your stay in this super comfortable bedroom connected with a spacious private living room with lots of natural light. Direct access to balcony and private stylish bathroom (outside the room). The room has a private open kitchen equipped with electric cooktop, kettle, fridge and washing machine. A dining area outside the room is also available for use.',
@@ -61,9 +84,11 @@ flat = Flat.create!(
   number_of_guests: rand(2..4),
   url: "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2167&q=80"
 )
-puts "Created flat with id #{flat.id}"
+flat.user = User.all.sample
+flat.save!
+puts "Created flat with id #{flat.id} - Belongs to user: #{flat.user.email}"
 
-flat = Flat.create!(
+flat = Flat.new(
   name: 'Departamento entero - Anfitrión: Valeria',
   address: 'Palermo, Buenos Aires, Argentina',
   description: 'Excelente departamento ubicado en PALERMO!! Súper equipado!! Cama KING SIZE, SMART TV, ropa de cama, toallas, cafetera, microondas, heladera, tostadora, pava eléctrica, utensilios de cocina, plancha y todo lo necesario para que te sientas como en tu casa!!.',
@@ -71,4 +96,6 @@ flat = Flat.create!(
   number_of_guests: rand(2..4),
   url: "https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2550&q=80"
 )
-puts "Created flat with id #{flat.id}"
+flat.user = User.all.sample
+flat.save!
+puts "Created flat with id #{flat.id} - Belongs to user: #{flat.user.email}"
