76278e138d5e79e8ef7a516c31082d5832580c3d
Nadia Salmen <nadiasalmen@gmail.com>
2021-02-19 16:58:59 -0300

    Add user authentication with devise

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,6 @@
 class FlatsController < ApplicationController
   before_action :set_flat, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:query].present?
