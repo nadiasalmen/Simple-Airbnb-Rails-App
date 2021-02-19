670e1145014a490398caf14fc7cc810a095b95a5
Nadia Salmen <nadiasalmen@gmail.com>
2021-02-19 16:34:40 -0300

    Install devise

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
 
+# Add devise
+gem 'devise'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  # Ensure you have defined default url options in your environments files. Here is an example of default_url_options appropriate for a development environment
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
