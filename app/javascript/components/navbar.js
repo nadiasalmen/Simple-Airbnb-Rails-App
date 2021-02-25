5c14a79953f765e7dbf100c45ea5881e7c1809a8
Nadia Salmen <nadiasalmen@gmail.com>
2021-02-24 23:44:47 -0300

    Add dynamic navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,6 +1,7 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
-  if (navbar) {
+  const homepage = document.querySelector('.home-cards');
+  if (navbar && homepage) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-lewagon-white');
@@ -8,6 +9,10 @@ const initUpdateNavbarOnScroll = () => {
         navbar.classList.remove('navbar-lewagon-white');
       }
     });
+  }
+  if (homepage === null) {
+    navbar.classList.add('navbar-lewagon-white');
+    navbar.classList.remove('fixed-top');
   }
 }
 
